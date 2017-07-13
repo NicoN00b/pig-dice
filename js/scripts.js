d96ac60df4b6755e20767963db44214e74ceb993
nr <nicholas.raethke@gmail.com>
2017-07-12 18:04:24 -0700

    business logic addition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,37 @@
 
-// function Player(player1) {
-//   this.player1 =
-// }
+function Player(player1) {
+  this.player = playerName;
+  this.score = score;
+  this.turnScore = turnScore;
+}
 
-// var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+Player.prototype.rollDie = function(){
+  var rollScore = 0
+  var diceArray = []
+  var dice1 = Math.floor(Math.random() * (7 - 1)+1);
+  var dice2 = Math.floor(Math.random() * (7 - 1)+1);
+  // This might be a good alternative: var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+  if (dice1 !== 1 && dice2 !== 1){
+    var totalDice = dice1 + dice2;
+    rollScore = totalDice;
+  } else {
+    rollScore = 0 ;
+    this.turnScore;
+    this.turnScore += 0;
+    return "Womp Womp Womp";
+  }
+
+  diceArray(dice1, dice2);
+  return diceArray;
+};
+
+Player.prototype.hold = function() {
+  this.score += this.turnScore
+  this.turnScore = 0;
+}
+
+
 //
 // Player2
 //
